1a8772ab1a4ac4b1ce6703f4fbd832827e1379b0
Masuda <a.21m.4x@gmail.com>
2020-10-06 04:30:39 +0000

    practice2_commit2

--- practice2.php
+++ practice2.php
@@ -12,19 +12,21 @@ for ($i = 0 ; $i > 10000 ; $i++){
 }
 echo "\n";
 
-$fruits =array("apple","orange","banana","mango","painapple")
+$fruits =array("apple","orange","banana","mango","painapple");
 foreach ($fruits as $fruits){
-    echo $fruits
-};
-echo "\n"
+    echo $fruits;
+    echo "\n";
+}
+echo "\n";
 
-/* for文の始めの値を定義する /
+/* for文の始めの値を定義する */
 $start = 1;
 /* for文の終わりの値を定義する */
-for($i = 1 ; $i<100 ; $i++){
+for($i = 1 ; $i<=100 ; $i++){
 
   // 5で割り切れたら{}内を実行する
-  if($i / 5 > 0){
-    echo $i
-  };
-};
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
